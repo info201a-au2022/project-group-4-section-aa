4fc972375a5d5dfa7dc2c6b46ac65d8c803aa4fd
shuneng <shuneng@uw.edu>
2022-11-15 01:08:30 -0800

    Aggregate_Table

--- source/Aggregate_Table.R
+++ source/Aggregate_Table.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(knitr)
 
 concentrations_particulate_matter <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-aa/main/data/Concentrations%20of%20fine%20particulate%20matter.csv")
 
@@ -16,12 +17,12 @@ new_clean_fuels <- clean_fuels %>%
 ## 2019 clean fuels reliance rates on average for each region
 avg_reliance_region <- new_clean_fuels %>% 
   group_by(Region) %>% 
-  summarise(avg_rate = mean(Rates, na.rm = TRUE, round(2)))
+  summarise(avg_clean_fuels_rate = mean(Rates, na.rm = TRUE, round(2)))
 
 ## 2019 clean fuels reliance rates on average for each countries
 avg_reliance_countries <- new_clean_fuels %>% 
   group_by(Countries) %>% 
-  summarise(avg_rate = mean(Rates, na.rm = TRUE, round(2)))
+  summarise(avg_clean_fuels_rate = mean(Rates, na.rm = TRUE, round(2)))
 
 
 # mortality data values primarily use 
@@ -32,12 +33,12 @@ new_mortaity <- mortality %>%
 ## mortality rates on average for each region
 avg_mortality_region <- new_mortaity %>% 
   group_by(Region) %>% 
-  summarise(avg_rate = mean(Rates, na.rm = TRUE, round(2)))
+  summarise(avg_mortality_rate = mean(Rates, na.rm = TRUE, round(2)))
 
 ## mortality rates on average for each countries
 avg_mortality_countries <- new_mortaity %>% 
   group_by(Countries) %>% 
-  summarise(avg_rate = mean(Rates, na.rm = TRUE, round(2)))
+  summarise(avg_mortality_rate = mean(Rates, na.rm = TRUE, round(2)))
 
 
 # Concentrations particulate matter data values primarily use
@@ -48,9 +49,27 @@ new_concentrations_particulate_matter <- concentrations_particulate_matter %>%
 ## Concentrations particulate matter on average for each region
 avg_concentrations_region <- new_concentrations_particulate_matter %>% 
   group_by(Region) %>% 
-  summarise(avg_rate = mean(Rates, na.rm = TRUE, round(2)))
+  summarise(avg_concentration_rate = mean(Rates, na.rm = TRUE, round(2)))
 
 ## Concentrations particulate matter on average for each countries
 avg_Concentrations_countries <- new_concentrations_particulate_matter %>% 
   group_by(Countries) %>% 
-  summarise(avg_rate = mean(Rates, na.rm = TRUE, round(2)))
+  summarise(avg_concentration_rate = mean(Rates, na.rm = TRUE, round(2)))
+
+## summary of all three average rate for regions
+summary_values_region <- left_join(avg_concentrations_region, 
+                                   avg_mortality_region, 
+                                   by = "Region") %>% 
+  mutate(avg_reliance_rate = avg_reliance_region$avg_clean_fuels_rate)
+
+## output as an table
+kable(summary_values_region)
+
+## summary of all three average rate for countries
+summary_values_countries <- left_join(avg_reliance_countries, 
+                                      avg_Concentrations_countries, 
+                                      by = "Countries") %>% 
+  left_join(avg_mortality_countries, by = "Countries")
+
+## output as an table
+kable(summary_values_countries)
